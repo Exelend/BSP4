c466c6aac9a9dcb6e5566abe3e6a33bf9702d793
BS <bs@linux-o61u.suse>
2018-01-02 20:09:50 +0100

    mutex anfang

--- src/tzm.c
+++ src/tzm.c
@@ -8,27 +8,35 @@
 #include <linux/moduleparam.h>
 #include <linux/init.h>
 
-#include <linux/kernel.h>	/* printk() */
-#include <linux/slab.h>		/* kmalloc() */
-#include <linux/fs.h>		/* everything... */
-#include <linux/errno.h>	/* error codes */
-#include <linux/types.h>	/* size_t */
-
+#include <linux/kernel.h>	// printk()
+#include <linux/slab.h>		// kmalloc()
+#include <linux/fs.h>		// everything...
+#include <linux/errno.h>	// error codes
+#include <linux/types.h>	// size_t
 #include <linux/jiffies.h>  // for Timer-Ticks
+#include <pthread. h.>      // Mutex, ...
 
+#include "tzm.h"
+
+static int ret_val_time = -1;
+static int ret_val_number = -1;
 static u64 last_newLine = 0;
 static u64 last_duration = 0;
-static int counter = 0;
+static int counter = ret_val_number;
+static int open_devices = ret_val_time;
+
+pthread_mutex_t mutex;
+
 
 /**
  * callback Funktionen zuweisen
  */
 static struct file_operations fops =
 {
-   //.open = dev_open,
+   .open = tzm_open,
    .read = tzm_read,
    .write = tzm_write,
-   //.release = dev_release,
+   //.release = tzm_release,
 };
 
 int tzm_inital( void ){
@@ -39,11 +47,17 @@ int tzm_inital( void ){
         return majorNumber;
     }
     printk(KERN_INFO "tzm Module loaded.\n");
-    last_newLine = get_jiffes_64();
+    last_newLine = get_jiffies_64();
+    if(pthread_mutex_init (&mutex, NULL) != 0 ){
+        printk(KERN_ALERT "Failed mutex_init\n");
+        return EXIT_FAILURE;
+    }
+    PDEBUG("tzm_inital -> OK"); // Evtl. Fehler: Keine Parameter übergeben.
     return EXIT_SUCCESS;
 }
 
 void tzm_exit( void ){
+    pthread_mutex_destroy(&mutex);
     printk(KERN_INFO "tzm Module unloaded.\n");
 }
 
@@ -52,8 +66,8 @@ ssize_t tzm_write(struct file *filp, const char __user *buf, size_t bufSize, lof
     for(int i = 0 ; i < bufSize; i++){
         counter++;
         if(buf[i] == '\n'){
-            last_duration = get_jiffes_64() - last_newLine;
-            printk(KERN_INFO "Ticks: %d \nSigns: %d\n", last_duration, counter); // Evtl Fehlerquelle!!!!!!! (Formatierung)
+            last_duration = get_jiffies_64() - last_newLine;
+            printk(KERN_INFO "Time: % " PRIu64 "\nSigns: %d\n", ((uint64_t)last_duration)/HZ, counter); // Evtl Fehlerquelle!!!!!!! (Formatierung)
             return counter;
         }    
     }
@@ -61,8 +75,19 @@ ssize_t tzm_write(struct file *filp, const char __user *buf, size_t bufSize, lof
 }
 
 ssize_t tzm_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos ){
-    printk(KERN_INFO "Ticks: %d \nSigns: %d\n", last_duration, counter); // Evtl Fehlerquelle!!!!!!! (Formatierung)
+    printk(KERN_INFO "Time: % " PRIu64 "\nSigns: %d\n", ((uint64_t)last_duration)/HZ, counter); // Evtl Fehlerquelle!!!!!!! (Formatierung)
     return counter;
+}    
+
+/*
+ * Zur überwachung der 'Geräteanzahl' (MAX 1 Gerät!)
+ */ 
+int tzm_open(struct inode *, struct file *){
+    if(open_devices != 0){
+        return EBUSY;
+    }
+    open_devices++;
+    return EXIT_SUCCESS;
 }    
 
 module_init(tzm_initial);
---
+++ src/tzm.h
@@ -0,0 +1,25 @@
+/*
+ * Header-Datei zu tzm.c, BSP 4
+ * 
+ * Hauke Goldhammer, Martin Witte
+ */
+
+#ifndef TZM_H_
+
+#define TZM_H_
+
+
+//#undefine DEBUG_ON
+#define DEBUG_ON 
+
+
+#undef PDEBUG
+
+#ifdef DEBUG_ON
+#   define PDEBUG(msg, args...) printk( KERN_DEBUG "tzm: " msg, ##args)
+#else
+#   define PDEBUG(msg, args...) // kein debugging
+#endif // DEBUG_ON
+
+
+#endif // TZM_H_
